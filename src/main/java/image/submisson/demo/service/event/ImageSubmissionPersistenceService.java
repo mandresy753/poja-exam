95aa9d6b081bcd9cc70a54e7f368e46a7a68195f
Mandresy Gaëtan RANDRIANANTOANINA <mandresy2020@gmail.com>
2026-07-13 10:01:26 +0300

    feat: add submissions services

---
+++ src/main/java/image/submisson/demo/service/event/ImageSubmissionPersistenceService.java
@@ -0,0 +1,49 @@
+package image.submisson.demo.service;
+
+
+import image.submisson.demo.mapper.ImageSubmissionMapper;
+import image.submisson.demo.model.ImageSubmission;
+import image.submisson.demo.repository.ImageSubmissionRepository;
+import image.submisson.demo.repository.model.JImageSubmission;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.UUID;
+
+
+@Service
+@AllArgsConstructor
+public class ImageSubmissionPersistenceService {
+
+
+    private final ImageSubmissionRepository repository;
+
+    private final ImageSubmissionMapper mapper;
+
+
+
+    public ImageSubmission save(
+            UUID id,
+            String filename,
+            String email
+    ){
+
+
+        JImageSubmission entity =
+                JImageSubmission.builder()
+                        .id(id)
+                        .nomFichier(filename)
+                        .email(email)
+                        .createdAt(Instant.now())
+                        .build();
+
+
+
+        return mapper.toModel(
+                repository.save(entity)
+        );
+
+    }
+
+}
