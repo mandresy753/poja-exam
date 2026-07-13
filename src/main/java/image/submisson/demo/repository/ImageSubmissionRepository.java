21eaa82d7069b59b939a79fdd8a459134453d913
Mandresy Gaëtan RANDRIANANTOANINA <mandresy2020@gmail.com>
2026-07-13 09:49:59 +0300

    feat: add ImageSubmissionRepository

---
+++ src/main/java/image/submisson/demo/repository/ImageSubmissionRepository.java
@@ -0,0 +1,11 @@
+package image.submisson.demo.repository;
+
+import image.submisson.demo.repository.model.JImageSubmission;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ImageSubmissionRepository
+        extends JpaRepository<JImageSubmission, UUID> {
+
+}
